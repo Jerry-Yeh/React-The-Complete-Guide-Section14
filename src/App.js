8bdea31212c447205b53038a444ed7114a4d0180
jerry <qaz013039@gmail.com>
2022-07-11 23:22:01 +0800

    Rewrite JSX logic with JavaScript

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const fetchMoviesHandler = async () => {
     try {
       setIsLoading(true);
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch("https://swapi.dev/api/film");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -36,17 +36,20 @@ function App() {
     setIsLoading(false);
   };
 
+  let content = <p>Found no movies!</p>;
+
+  if (movies.length > 0) content = <MoviesList movies={movies} />;
+
+  if (error) content = <p>{error}</p>;
+
+  if (isLoading) content = <p>Loading...</p>;
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies!</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
